cf3f41d962eab4190d21ea7785ea6d30811e2b6b
Matt Emborsky <memborsky@gmail.com>
2012-12-03 19:41:49 -0500

    Use a django template for the poll index page.

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,10 @@
+from django.shortcuts import render_to_response
 from polls.models import Poll
 from django.http import HttpResponse
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    output = ', '.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
