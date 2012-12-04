d4e57139adca5f02cb4a04d2e8a7a97a0b98cc68
Matt Emborsky <memborsky@gmail.com>
2012-12-03 19:04:38 -0500

    Add 4 view routes. /polls/ (index view) :: /polls/<id>/ (detail view) :: /polls/<id>/results/ (results view) :: /polls/<id>/vote/ (voting results view). We currently output basic information on each view.

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -5,6 +5,18 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # Poll index
+    url(r'^polls/$', 'polls.views.index'),
+
+    # Detailed poll view
+    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+
+    # The results of the poll
+    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+
+    # Vote on the poll
+    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+
     # Examples:
     # url(r'^$', 'tutorial.views.home', name='home'),
     # url(r'^tutorial/', include('tutorial.foo.urls')),
